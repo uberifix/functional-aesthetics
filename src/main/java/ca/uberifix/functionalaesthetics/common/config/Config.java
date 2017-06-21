57238e62f037a5410692ad2f9ded33b5b6bd1c03
uberifix <uberifix@gmail.com>
2017-06-21 15:09:20 -0400

    Improvements to campfire cooking
    - Campfire cooking is now more consistent
    - Campfires no longer toss items everywhere
    - Configs to disable cooking completely, and the time and amount of items it can cook
    - Issue of items going out of range and being unable to be picked up is still there, but now is less likely to happen without external forces
    - Item burning still needs to be added

--- src/main/java/ca/uberifix/functionalaesthetics/common/config/Config.java
+++ src/main/java/ca/uberifix/functionalaesthetics/common/config/Config.java
@@ -8,13 +8,17 @@ import net.minecraftforge.common.config.Configuration;
  * Created by uberifix
  */
 public class Config {
-
-    private static final String CATEGORY_MODULES = "All Modules";
-    private static final String CATEGORY_RUSTIC = "Rustic Module";
-    private static final String CATEGORY_MODERN = "Modern Module";
-
-    public static boolean RUSTIC_MODULE_ENABLED = true;
-    public static boolean MODERN_MODULE_ENABLED = true;
+    //Config Categories
+    private static final String CATEGORY_MODULES            = "All Modules";
+    private static final String CATEGORY_RUSTIC             = "Rustic Module";
+    private static final String CATEGORY_MODERN             = "Modern Module";
+    //Rustic Module Configs
+    public static boolean   RUSTIC_MODULE_ENABLED           = true;
+    public static boolean   RUSTIC_CAMPFIRE_COOKING         = true;
+    public static int       RUSTIC_CAMPFIRE_COOK_AMOUNT     = 4;
+    public static int       RUSTIC_CAMPFIRE_COOK_TIME       = 100;
+    //Modern Module Configs
+    public static boolean   MODERN_MODULE_ENABLED           = true;
 
     public static void readConfig() {
         Configuration cfg = CommonProxy.config;
@@ -34,13 +38,15 @@ public class Config {
 
     private static void initModuleConfig(Configuration cfg) {
         cfg.addCustomCategoryComment(CATEGORY_MODULES, "Enable or Disable Various Modules, see https://mods.uberifix.ca/FunctionalAesthetics for what each module includes");
-        RUSTIC_MODULE_ENABLED = cfg.getBoolean("rusticModuleEnabled", CATEGORY_MODULES, RUSTIC_MODULE_ENABLED, "Enable/Disable the Rustic Module");
-        MODERN_MODULE_ENABLED = cfg.getBoolean("modernModuleEnabled", CATEGORY_MODULES, MODERN_MODULE_ENABLED, "Enable/Disable the Modern Module");
+        RUSTIC_MODULE_ENABLED           = cfg.getBoolean("Enable Rustic Module", CATEGORY_MODULES, RUSTIC_MODULE_ENABLED, "Enable/Disable the Rustic Module");
+        MODERN_MODULE_ENABLED           = cfg.getBoolean("Enable Modern Module", CATEGORY_MODULES, MODERN_MODULE_ENABLED, "Enable/Disable the Modern Module");
     }
 
     private static void initRusticConfig(Configuration cfg) {
         cfg.addCustomCategoryComment(CATEGORY_RUSTIC, "Customize the components included in the rustic module");
-
+        RUSTIC_CAMPFIRE_COOKING         = cfg.getBoolean("Enable Campfire Cooking", CATEGORY_RUSTIC, RUSTIC_CAMPFIRE_COOKING, "Enable cooking at a campfire by throwing food near it");
+        RUSTIC_CAMPFIRE_COOK_AMOUNT     = cfg.getInt("Campfire Cooking Amount", CATEGORY_RUSTIC, RUSTIC_CAMPFIRE_COOK_AMOUNT, 1, 16, "Amount of items campfires can cook simultaneously");
+        RUSTIC_CAMPFIRE_COOK_TIME       = cfg.getInt("Campfire Cooking Time", CATEGORY_RUSTIC, RUSTIC_CAMPFIRE_COOK_TIME, 20, 1200,"Time it takes for food to cook at a campfire, in ticks");
     }
 
     private static void initModernConfig(Configuration cfg) {
--- src/main/java/ca/uberifix/functionalaesthetics/common/tileentity/rustic/CampfireTileEntity.java
+++ src/main/java/ca/uberifix/functionalaesthetics/common/tileentity/rustic/CampfireTileEntity.java
@@ -1,9 +1,9 @@
 package ca.uberifix.functionalaesthetics.common.tileentity.rustic;
 
+import ca.uberifix.functionalaesthetics.FunctionalAesthetics;
 import ca.uberifix.functionalaesthetics.common.tileentity.TileEntityCommon;
 import mcjty.lib.tools.ItemStackTools;
 import net.minecraft.entity.item.EntityItem;
-import net.minecraft.init.Items;
 import net.minecraft.item.ItemFood;
 import net.minecraft.item.ItemStack;
 import net.minecraft.item.crafting.FurnaceRecipes;
@@ -12,62 +12,60 @@ import net.minecraft.util.ITickable;
 import net.minecraft.util.math.AxisAlignedBB;
 import net.minecraft.world.WorldServer;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
+
+import static ca.uberifix.functionalaesthetics.common.config.Config.RUSTIC_CAMPFIRE_COOKING;
+import static ca.uberifix.functionalaesthetics.common.config.Config.RUSTIC_CAMPFIRE_COOK_AMOUNT;
+import static ca.uberifix.functionalaesthetics.common.config.Config.RUSTIC_CAMPFIRE_COOK_TIME;
 
 /**
  * Created by uberifix
  */
 public class CampfireTileEntity extends TileEntityCommon implements ITickable{
+    private final Map<UUID, Integer> itemsCooking = new HashMap<>();
     private int tick;
-    private int processTime;
+    private int currentlyCooking;
+
     @Override
     public void update () {
-        tick = (tick + 1) %20;
+        tick = (tick + 1) % 20;
 
-        List<EntityItem> itemsToCook = new ArrayList<>();
-        if (tick == 0 && !getWorld().isRemote) {
-            double x = this.pos.getX(), y = this.pos.getY(), z = this.pos.getZ();
-            AxisAlignedBB COOK_RADIUS_AABB = new AxisAlignedBB(x - 1, y, z - 1, x + 2, y + 1, z + 2);
+        if (isInvalid() || !getWorld().isBlockLoaded(getPos()) || getWorld().isRemote || !RUSTIC_CAMPFIRE_COOKING) { return; }
+
+        try {
+            double blockX = this.pos.getX(), blockY = this.pos.getY(), blockZ = this.pos.getZ();
+            AxisAlignedBB COOK_RADIUS_AABB = new AxisAlignedBB(blockX - 1, blockY, blockZ - 1, blockX + 2, blockY + 0.01, blockZ + 2);
             List<EntityItem> itemsNearby = getWorld().getEntitiesWithinAABB(EntityItem.class, COOK_RADIUS_AABB);
             for (EntityItem itemNearby : itemsNearby) {
                 double itemX = itemNearby.posX, itemY = itemNearby.posY, itemZ = itemNearby.posZ;
-                int itemsToCookAvailable = 4 - itemsToCook.size();
-                ItemStack cookedItem = FurnaceRecipes.instance().getSmeltingResult(itemNearby.getEntityItem());
-                if(!itemNearby.cannotPickup()) {
-                    if (!ItemStackTools.isEmpty(cookedItem)) {
-                        if (cookedItem.getItem() instanceof ItemFood) {
-                            //TODO: limit items cooked to 8, add cook timer, add steam particle effects during cooking
-                            //TODO: after cooked, add smoke particles, after 2 sec, add fire particles, after 2 sec turn item to charcoal
-                            if (itemsToCookAvailable > 0) {
-                                EntityItem newItem = new EntityItem(getWorld(), itemX, itemY+0.2, itemZ, itemNearby.getEntityItem().splitStack(1));
-                                newItem.setInfinitePickupDelay();
-                                getWorld().spawnEntity(newItem);
-                                itemsToCook.add(newItem);
-                            }
+                UUID itemUUID = itemNearby.getPersistentID();
+                ItemStack itemNearbyCooked = FurnaceRecipes.instance().getSmeltingResult(itemNearby.getEntityItem());
+                boolean itemNearbyIsFood = itemNearbyCooked.getItem() instanceof ItemFood;
+                if (!ItemStackTools.isEmpty(itemNearbyCooked)) {
+                    if (itemNearbyIsFood && currentlyCooking < RUSTIC_CAMPFIRE_COOK_AMOUNT && !this.itemsCooking.containsKey(itemUUID)) {
+                        EntityItem newItem = new EntityItem(getWorld(), itemX, itemY, itemZ, itemNearby.getEntityItem().splitStack(1));
+                        newItem.setInfinitePickupDelay();
+                        newItem.setVelocity(0, 0, 0);
+                        getWorld().spawnEntity(newItem);
+                        this.itemsCooking.put(newItem.getPersistentID(), RUSTIC_CAMPFIRE_COOK_TIME);
+                        currentlyCooking++;
+                    } else if (this.itemsCooking.containsKey(itemUUID)) {
+                        this.itemsCooking.put(itemUUID, this.itemsCooking.get(itemUUID) - 1);
+                        if (tick % 5 == 0) {
+                            ((WorldServer) getWorld()).spawnParticle(EnumParticleTypes.EXPLOSION_NORMAL, true, itemX, itemY + 0.5, itemZ, 1, 0, 0, 0, 0.01f, new int[0]);
+                        }
+                        if (this.itemsCooking.get(itemUUID) <= 0) {
+                            itemNearby.setEntityItemStack(itemNearbyCooked.copy());
+                            itemNearby.setNoPickupDelay();
+                            ((WorldServer) getWorld()).spawnParticle(EnumParticleTypes.END_ROD, true, itemX, itemY+0.5, itemZ, 5, 0, 0, 0, 0.01f, new int[0]);
+                            currentlyCooking--;
+                            this.itemsCooking.remove(itemUUID);
                         }
                     }
-                } else { itemsToCook.add(itemNearby); }
+                } //else add to burn map
             }
-        }
-        if (tick % 5 == 0 && !getWorld().isRemote) {
-            if(itemsToCook.size() > 0) { processTime = (processTime + 1) % 8; }
-            if (processTime == 0) {
-                for (EntityItem itemToCook : itemsToCook) {
-                    double itemX = itemToCook.posX, itemY = itemToCook.posY, itemZ = itemToCook.posZ;
-                    ItemStack itemCooked = FurnaceRecipes.instance().getSmeltingResult(itemToCook.getEntityItem());
-                    ItemStackTools.setStackSize(itemCooked, ItemStackTools.getStackSize(itemToCook.getEntityItem()));
-                    itemToCook.setEntityItemStack(itemCooked.copy());
-                    itemToCook.setNoPickupDelay();
-                    ((WorldServer) this.getWorld()).spawnParticle(EnumParticleTypes.END_ROD, true, itemX, itemY+1, itemZ, 20, 0, 0, 0, 0.01f, new int[0]);
-                }
-            }
-        }
-        if (this.getWorld() instanceof WorldServer) {
-            for (EntityItem itemToCook : itemsToCook) {
-                double itemX = itemToCook.posX, itemY = itemToCook.posY, itemZ = itemToCook.posZ;
-                ((WorldServer) this.getWorld()).spawnParticle(EnumParticleTypes.EXPLOSION_NORMAL, true, itemX, itemY+0.5, itemZ, 3, 0, 0, 0, 0.01f, new int[0]);
-            }
+        } catch (final Exception e) {
+            FunctionalAesthetics.logger.warn(e);
         }
     }
 }
